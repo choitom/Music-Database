943bf24c9b4b43c9c0eca873177855298222be98
choitom <choito@carleton.edu>
2016-08-03 17:09:05 -0500

    Music Database upload

---
+++ createtable.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS top6000songs;
+CREATE TABLE top6000songs (
+    songRank int,
+    songArtist VARCHAR(255),
+    songTitle VARCHAR(255),
+    songYear int
+);
---
+++ datasource.py
@@ -0,0 +1,57 @@
+class MusicDataSource:
+	def __init__(self, ...):
+		pass
+	
+	def getSongTitles(self):
+		'''
+		Returns a list of distinct titles of all songs in the database
+		'''
+		return []
+	
+	def getArtists(self):
+		'''
+		Returns a list of distinct artists of all songs in the database
+		'''
+		return []
+		
+	def getYearRange(self):
+		'''
+		Returns a list of distinct years of all songs in the database.
+		'''
+		return []
+		
+	def getSongsForTitle(self, title):
+		'''
+		Returns a list of songs with the given title.
+		'''	
+		return []
+	
+	def getSongsForArtist(self, artist):
+		'''
+		Returns a list of the songs of the given artist.
+		'''
+		return []
+	
+	def getSongsForYearRange(self, yearBegin, yearEnd):
+		'''
+		Returns a list of songs within the year range.
+		'''
+		return []
+	
+	def getSongsForRanking(self, rankBegin, rankEnd):
+		'''
+		Returns a list of songs within the rank range.
+		'''
+		return []
+		
+	def getMacallanTopTen(self):
+		'''
+		Returns a list of ten songs that Macallan likes the most
+		'''
+		return []
+	
+	def getTomTopTen(self):
+		'''
+		Returns a list of ten songs that Tom likes the most
+		'''
+		return []
